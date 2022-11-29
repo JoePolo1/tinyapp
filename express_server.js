1932779c8f9f15d100ebc615386d96b1bb24d65e
Joe Polo <joe.polo@telus.com>
2022-11-28 17:06:24 -0800

    added route functionality

--- express_server.js
+++ express_server.js
@@ -22,9 +22,14 @@ app.get("/urls", (req, res) =>  {
   res.render("urls_index", templatevars);
 })
 
-app.get("/urls show", (req, res)  =>  {
-  //this is a new route which should direct to the urls_show page... now, what to do with it?
-})
+app.get("/urls/:id", (req, res) => {
+  console.log(req.params);
+  const templateVars = { 
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+   };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/set", (req, res) => {
   const a = 1;
